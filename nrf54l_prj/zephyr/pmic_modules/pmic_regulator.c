31a3491c8f488684e5394e1a1be9888c161c6355
smith.hu <smith.a1672@gmail.com>
2025-05-25 12:08:31 +0800

    Update pmic_regulator.c

--- nrf54l_prj/zephyr/pmic_modules/pmic_regulator.c
+++ nrf54l_prj/zephyr/pmic_modules/pmic_regulator.c
@@ -4,3 +4,69 @@
  * @date:      2025-05-25
  * @copyright: Copyright (c) 2025 Motive Technologies, Inc. All rights reserved.
  */
+
+#include <zephyr/logging/log_ctrl.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/mfd/npm2100.h>
+#include <zephyr/drivers/regulator.h>
+
+static const struct device *pmic = DEVICE_DT_GET(DT_NODELABEL(npm2100_pmic));
+static const struct device *regulators = DEVICE_DT_GET(DT_NODELABEL(npm2100_regulators));
+
+static void event_callback(const struct device *dev, struct gpio_callback *cb, uint32_t events)
+{
+	static int64_t press_t;
+
+	if (events & BIT(NPM2100_EVENT_SYS_SHIPHOLD_FALL)) {
+		press_t = k_uptime_get();
+	}
+
+	if (events & BIT(NPM2100_EVENT_SYS_SHIPHOLD_RISE)) {
+		int64_t delta_t = k_uptime_get() - press_t;
+
+		if (delta_t < PRESS_SHORT_MS) {
+			printk("Short press\n");
+			flash_time_ms = FLASH_FAST_MS;
+		} else if (delta_t < PRESS_MEDIUM_MS) {
+			printk("Medium press\n");
+			flash_time_ms = FLASH_SLOW_MS;
+		} else {
+			shipmode = true;
+		}
+	}
+}
+
+static bool configure_events(void)
+{
+	int ret;
+	static struct gpio_callback event_cb;
+
+	if (!device_is_ready(pmic)) {
+		printk("Error: PMIC device not ready.\n");
+		return false;
+	}
+
+	if (!device_is_ready(regulators)) {
+		printk("Error: Regulator device not ready.\n");
+		return false;
+	}
+
+	gpio_init_callback(&event_cb, event_callback,
+			   BIT(NPM2100_EVENT_SYS_SHIPHOLD_FALL) |
+				   BIT(NPM2100_EVENT_SYS_SHIPHOLD_RISE));
+
+	ret = mfd_npm2100_add_callback(pmic, &event_cb);
+
+	if (ret < 0) {
+		printk("Error: failed to add a PMIC event callback.\n");
+		return false;
+	}
+
+	return true;
+}
+
+int pmic_regulator_ship_mode(void)
+{
+    return 0;
+}
