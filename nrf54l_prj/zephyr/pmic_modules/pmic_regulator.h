aa84abf966ca8f18b3808d140d9dac76fb1aebf1
smith.hu <smith.a1672@gmail.com>
2025-05-25 12:02:27 +0800

    Update pmic_regulator.h

---
+++ nrf54l_prj/zephyr/pmic_modules/pmic_regulator.h
@@ -0,0 +1,6 @@
+/**
+ * @brief:     PMIC NPM2100 module
+ * @file:      pmic_regulator.h
+ * @date:      2025-05-25
+ * @copyright: Copyright (c) 2025 Motive Technologies, Inc. All rights reserved.
+ */
